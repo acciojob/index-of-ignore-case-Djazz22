b352ccf77ba734cba9e732729b3647c75af521b1
Djazz22 <106832875+Djazz22@users.noreply.github.com>
2024-06-29 20:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+    // Convert both strings to lower case for case-insensitive comparison
+    var lowerStr = str.toLowerCase();
+    var lowerSubStr = subStr.toLowerCase();
+
+    // Find the first occurrence of the lowercased subStr in the lowercased str
+    var index = lowerStr.indexOf(lowerSubStr);
+
+    return index;
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var mainString = document.getElementById('mainString').value;
+    var subString = document.getElementById('subString').value;
+    var result = indexOfIgnoreCase(mainString, subString);
+
+    document.getElementById('result').innerText = 'Result: ' + result;
+});
